41cea6743ad218957a99b1c312fc201ccc8a14b6
lucas-besen <lucas.andrebesen@gmail.com>
2020-07-20 23:17:33 -0300

    template do corriculo virtual imagem e icons que redireciona para as redes sociais

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+  	<meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+      <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Trade+Winds&display=swap" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="icons/font.css">
+      <link rel="stylesheet" type="text/css" href="icons/main.css">
+      <style>
+        body{
+        
+          background-color:#666666;   
+        }   
+        img{
+          border-radius:5%;
+          width:60%;
+          height:60%;         
+        }
+        #centro {
+          position:absolute;
+          top: 10%;
+          left:75%;   
+        }
+        .retangulo{
+          width: 80%;
+          height: 70%;
+          background-color: #ffffff;
+          position: absolute;
+          top: 25%;
+          right: 15%;
+        }
+      </style>
+  </head>
+  <body>
+    <div class="container">
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include_once("header.php"); ?>
+
+
+  
+ 
+<div class="retangulo"></div>
+
+
+<div id="centro" >
+	<img src="fotos/lucas.jpg" >
+</div>
+<div class="social-bar">
+	<a href="https://github.com/Lucas-Besen"class="icon icon-github"></a>
+	<a href="https://www.linkedin.com/in/lucas-andr%C3%A9-besen-b52b23166/"class="icon icon-linkedin"></a>
+	<a href="mailto:lucas.andrebesen@gmail.com" class="icon icon-mail"></a>
+	<a href="https://www.instagram.com/lucas_besen/"class="icon icon-instagram"></a>
+	<a href="https://www.facebook.com/lucas.besen.52"class="icon icon-facebook""></a>	
+</div>
+
+<?php include_once("footer.php"); ?>
+
+
+
